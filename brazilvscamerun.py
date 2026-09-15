2ec90095456acc2e8819a96a33d8f1895c2d9514
Paula Sofia Bilbao Cruz <sofia.bilbao@ufm.edu>
2026-09-15 09:09:25 -0600

    Grafo creado

--- brazilvscamerun.py
+++ brazilvscamerun.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import networkx as nx
+import matplotlib.pyplot as plt
 
 #Leer datos ya filtrados de fase de grupos
 df = pd.read_csv("Pases_brasil_grupos.csv")
@@ -19,7 +20,7 @@ def crear_grafo(oponente, peso_minimo=3):
         .reset_index(name="peso")
     )
 
-    # Crear grafo dirigido
+    #Crear grafo dirigido
     G = nx.DiGraph()
     for _, fila in pases.iterrows():
         G.add_edge(
@@ -28,10 +29,41 @@ def crear_grafo(oponente, peso_minimo=3):
             weight=fila["peso"]
         )
 
-    print("Jugadores:", G.number_of_nodes())
-    print("Conexiones:", G.number_of_edges())
-    return G
+    
+    #Posición de los nodos
+    pos = nx.spring_layout(G, seed=42, k=0.8)
+ 
+    plt.figure(figsize=(12, 9))
+ 
+    #Dibujar nodos
+    nx.draw_networkx_nodes(G, pos, node_size=1800, node_color="#2E8B57")
+ 
+    #Dibujar nombres
+    nx.draw_networkx_labels(
+        G, pos,
+        labels={jugador: jugador.split()[0] for jugador in G.nodes()},
+        font_size=9
+    )
+ 
+    #Dibujar flechas
+    nx.draw_networkx_edges(
+        G, pos,
+        arrows=True,
+        arrowstyle="-|>",
+        arrowsize=18,
+        connectionstyle="arc3,rad=0.12",
+        edge_color="gray"
+    )
 
-
-#Grafo para el partido de Brasil vs Camerún
+     #Peso de cada flecha
+    pesos = nx.get_edge_attributes(G, "weight")
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=pesos, font_size=8, label_pos=0.5)
+ 
+    plt.title(f"Brasil vs {oponente} - Pases completados")
+    plt.axis("off")
+    plt.tight_layout()
+    plt.show()
+ 
+ 
+#Grafo de partido de Brasil vs Camerún
 crear_grafo("Cameroon")
