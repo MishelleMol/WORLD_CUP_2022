00e83bce0aa5128503e3e36f7e33898d1d5f90f2
MishelleMol <mishellemolinaarana@gmail.com>
2026-09-15 19:18:10 -0600

    Brasil vs Suiza

--- main.py
+++ main.py
@@ -129,3 +129,136 @@ plt.tight_layout()
 plt.savefig("Brasil_vs_Serbia.png", dpi=300, bbox_inches= "tight")
 #Mostramos la gráfica 
 plt.show()
+
+
+
+
+
+
+
+
+
+#3.3 Construcción del grafo - Brasil vs. Suiza
+
+suiza = df_grupos[df_grupos["oponente"] == "Switzerland"] #Extraemos de la columna oponente a Suiza
+
+#Solo pases completos
+suiza_completos = suiza[
+    suiza["resultado"] == "Complete"
+]
+
+grafo_suiza = nx.DiGraph() #Creamos grafo dirigido y lo guardamos
+
+for indice, fila in suiza_completos.iterrows(): #Cada fila contiene toda la información de un pase
+
+    #Guardamos quién hizo el pase y quién lo recibió
+    emisor = fila["jugador_nombre"]
+    receptor = fila["receptor_nombre"]
+
+    #Pregunta si ya existe una flecha desde emisor hasta receptor
+    if grafo_suiza.has_edge(emisor, receptor):
+        grafo_suiza[emisor][receptor]["weight"] += 1 #Si existe, significa que ya hubo un pase entre ellos
+                                                     #Entonces aumentamos el peso de la conexión en 1
+
+    else:
+        grafo_suiza.add_edge(emisor, receptor, weight=1) #Si no existe, significa que es el primer pase entre ellos
+                                                        #Creamos la conexión y empezamos su peso en 1
+
+
+peso_minimo = 4 #Peso mínimo para mostrar una conexión
+
+#Lista donde guardamos las conexiones que cumplan con el peso
+aristas_relevantes_suiza = []
+
+#Recorremos todas las conexiones del grafo
+for emisor, receptor, datos in grafo_suiza.edges(data=True):
+
+    #Revisamos si el peso de la conexión es mayor o igual al mínimo
+    if datos["weight"] >= peso_minimo:
+
+        #Si cumple, guardamos la conexión
+        aristas_relevantes_suiza.append((emisor, receptor))
+
+
+#Creamos un nuevo grafo que contiene solo las conexiones relevantes
+grafo_suiza_filtrado = grafo_suiza.edge_subgraph(aristas_relevantes_suiza).copy()
+
+
+#Calculamos la posición de cada jugador en el dibujo
+pos_suiza = nx.spring_layout(grafo_suiza_filtrado, seed=42, k=1.5)
+#Spring layout hace que NetworkX acomode a los jugadores intentando que el grafo quede distribuido
+#Seed hace que el dibujo no cambie de posición cada vez que se ejecuta el programa
+#K controla el espacio entre nodos
+
+
+plt.figure(figsize=(12,9)) #Creamos el espacio donde se dibujará el grafo
+
+#12 = ancho, 9 = alto (TAMAÑO DE LA HOJA)
+
+
+#Dibujamos los nodos de nuestro grafo
+nx.draw_networkx_nodes(
+    grafo_suiza_filtrado, #El grafo que dibujará usando el filtro de peso
+    pos_suiza, #Cada jugador en las coordenadas calculadas
+    node_size=1300, #Controla el tamaño de los círculos
+    node_color="#882E8B" #Define el color de los nodos
+)
+
+
+#Creamos nombres más cortos para que el grafo sea fácil de leer
+nombres_cortos_suiza = {
+    jugador: jugador.split()[0]
+    for jugador in grafo_suiza_filtrado.nodes()
+}
+
+
+#Mostramos el nombre de cada jugador sobre su nodo
+nx.draw_networkx_labels(
+    grafo_suiza_filtrado, #Queremos los nombres de los jugadores en el grafo filtrado
+    pos_suiza, #Para que NetworkX sepa dónde está cada jugador
+    labels=nombres_cortos_suiza,
+    font_size=8 #Tamaño de la letra
+)
+
+
+#Dibujamos las conexiones entre jugadores
+nx.draw_networkx_edges(
+    grafo_suiza_filtrado,
+    pos_suiza, #Coordenadas de los jugadores
+    arrows=True, #Indicamos que queremos flechas porque el grafo es dirigido
+    arrowstyle="-|>", #Define la forma de la punta de la flecha
+    arrowsize=18, #Define el tamaño de la punta
+    width=1, #Todas las conexiones tendrán el mismo grosor
+    connectionstyle="arc3,rad=0.20", #Hace que las flechas tengan una pequeña curva
+    edge_color="gray" #Flechas grises
+)
+
+
+#Obtenemos el peso de cada conexión
+pesos_suiza = nx.get_edge_attributes(grafo_suiza_filtrado, "weight")
+
+
+#Escribimos el peso sobre cada conexión
+nx.draw_networkx_edge_labels(
+    grafo_suiza_filtrado,
+    pos_suiza,
+    edge_labels=pesos_suiza, #La etiqueta que queremos mostrar es el peso
+    font_size=8,
+    label_pos=0.5 #Indica en qué parte de la conexión colocamos el número
+)
+
+
+#Título de la gráfica
+plt.title("Brasil vs Suiza - Pases completos")
+
+#Quitamos los ejes porque no son necesarios en un grafo
+plt.axis("off")
+
+#Ajustamos el contenido para que aproveche bien el espacio
+plt.tight_layout()
+
+#Guardamos el grafo como imagen
+plt.savefig("Brasil_vs_Suiza.png", dpi=300, bbox_inches="tight")
+
+#Mostramos la gráfica
+plt.show()
